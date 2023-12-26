d385c3c1ddc7fc57500b23a56f45ce4a6c152a73
ánh như <125172179+nguyendoanhu06@users.noreply.github.com>
2023-12-26 16:23:37 +0700

    Update cart.js

--- Cart/cart.js
+++ Cart/cart.js
@@ -20,17 +20,20 @@ function render(){
                         </tr>
                     </table>
                     <div class="remove">
-                        <a href=""><i class="fa-regular fa-trash-can"></i></a>
+                        <a href="" onclick="removeItem()"><i class="fa-regular fa-trash-can"></i></a>
                         <p>Remove</p>
                     </div>
                 </div>
                 <div class="cart-product-item-total">
-                    <p><strong>Total: ${Number(cart[i].item.price)*cart[i].sl}$</strong></p>
+                    <p><strong>Total: ${(Number(cart[i].item.price)*cart[i].sl).toLocaleString()}$</strong></p>
                 </div>
             </div>`
 
     }
     document.querySelector('.rendercontent').innerHTML=content
+    let subtotal = Subtotal();
+  document.querySelector('.subtotal').innerHTML = `Subtotal: ${subtotal}$`;
+
 }
 
 render()
@@ -51,4 +54,23 @@ function decreaseQuantity(item) {
     }
     localStorage.setItem('cart', JSON.stringify(cart));
     render();
+}
+
+function removeItem(item) {
+    let cart = JSON.parse(localStorage.getItem('cart'));
+    cart.splice(item, 1);
+    localStorage.setItem('cart', JSON.stringify(cart));
+    render();
+}
+
+function Subtotal() {
+    let cart = JSON.parse(localStorage.getItem('cart'));
+    let subtotal = 0;
+  
+    for (let i = 0; i < cart.length; i++) {
+        let product = cart[i].item;
+        subtotal += product.price * cart[i].sl;
+    }
+  
+    return subtotal.toLocaleString();
 }
